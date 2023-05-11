bc1b915734a3865ba7c7d82a8a64dc5a763fe218
thisiskhandev <hassankhan427726@gmail.com>
2023-05-12 02:01:28 +0500

    Fetch All Records

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "db_school") or die("Connection failed: " . mysqli_error($conn));
---
+++ fetch-all.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once "config.php";
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); // Public API *
+
+$sql = "SELECT * FROM students";
+
+$result = mysqli_query($conn, $sql) or die("Query failed: " . mysqli_error($conn));
+
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    // echo "<pre>";
+    // print_r($output);
+    // echo "</pre>";
+    echo json_encode($output);
+} else {
+    echo json_encode(array('message' => 'No record found.', 'status' => '404'));
+}
+
+// if (mysqli_num_rows($result) > 0) {
+//     foreach ($result as $keys) {
+//         // echo "<pre>";
+//         // print_r($keys);
+//         // echo "</pre>";
+
+//         $output = json_encode($keys) . ",";
+//         echo $output;
+//     }
+// }
